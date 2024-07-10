9772cc46701c27002e9befa43cc68121353bb578
jam089 <jam089@mail.ru>
2024-07-10 23:40:08 +0300

    add DBCfg

--- TODOapp/core/config.py
+++ TODOapp/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 
 
 class RunCfg(BaseModel):
@@ -12,9 +13,18 @@ class APICfg(BaseModel):
     prefix: str = "/api"
 
 
+class DBCfg(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunCfg = RunCfg()
     api: APICfg = APICfg()
+    db: DBCfg
 
 
 settings = Settings()
