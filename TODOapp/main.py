e714fe09a95da715757c0384d553372b0f548b09
jam089 <jam089@mail.ru>
2024-07-11 01:20:27 +0300

    add lifespan

--- TODOapp/main.py
+++ TODOapp/main.py
@@ -1,13 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
 
 from core.config import settings
+from core.models import db_helper
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    yield
+    await db_helper.dispose()
+
+
+todo_app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:todo_app",
         host=settings.run.host,
         port=settings.run.port,
         reload=settings.run.reload,
