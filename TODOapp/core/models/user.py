c3fbedc8e9c7a2a58d13aec49c1d4f0ca6d6eff6
jam089 <jam089@mail.ru>
2024-07-11 19:24:24 +0300

    add task relationship to user

--- TODOapp/core/models/user.py
+++ TODOapp/core/models/user.py
@@ -1,12 +1,17 @@
-from datetime import datetime
+from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, func
+from sqlalchemy import String
 
 from core.models import Base
+
+if TYPE_CHECKING:
+    from core.models import Task
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
     password: Mapped[str]
     active: Mapped[bool] = mapped_column(default=True, server_default="1")
+
+    tasks: Mapped[list["Task"]] = relationship(back_populates="user")
