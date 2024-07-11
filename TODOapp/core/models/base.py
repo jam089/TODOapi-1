2d5678317fdae6e129029b3b298284cf938632e4
jam089 <jam089@mail.ru>
2024-07-11 19:07:12 +0300

    add created_at and last_update_at to Base

--- TODOapp/core/models/base.py
+++ TODOapp/core/models/base.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from sqlalchemy import func
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, declared_attr
 
 from core.utils import camel_to_snake
@@ -11,3 +14,8 @@ class Base(DeclarativeBase):
         return f"{camel_to_snake(cls.__name__)}s"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.now(),
+        server_default=func.now(),
+    )
+    last_update_at: Mapped[datetime | None] = mapped_column(onupdate=func.now())
