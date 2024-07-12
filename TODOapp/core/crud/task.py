4f191de21396cb43d9a32f80a7843b2c4fcdf267
jam089 <jam089@mail.ru>
2024-07-12 18:15:13 +0300

    create task cruds, add get_all_tasks

---
+++ TODOapp/core/crud/task.py
@@ -0,0 +1,13 @@
+from typing import Sequence
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, Result, ScalarResult
+
+from core.models import Task
+from api.schemas import CreateTaskSchm, UpdateTaskSchm, ChangeTaskUserSchm
+
+
+async def get_all_tasks(session: AsyncSession) -> Sequence[Task]:
+    stmt = select(Task).order_by(Task.user_id)
+    result: ScalarResult = await session.scalars(stmt)
+    return result.all()
