f11d56fd90ee0864edb9d66288a4fef803fa7c7c
jam089 <jam089@mail.ru>
2024-07-12 19:59:30 +0300

    add get_task_by_id

--- TODOapp/core/crud/task.py
+++ TODOapp/core/crud/task.py
@@ -16,3 +16,7 @@ async def get_all_tasks(session: AsyncSession) -> Sequence[Task]:
     stmt = select(Task).order_by(Task.user_id, Task.id)
     result: ScalarResult = await session.scalars(stmt)
     return result.all()
+
+
+async def get_task_by_id(session: AsyncSession, task_id: int) -> Task | None:
+    return await session.get(Task, task_id)
