2ac225716c0b4c7fe64d9e1e45d994c58ef91a9f
jam089 <jam089@mail.ru>
2024-07-12 12:33:15 +0300

    add name and b_date to user model

--- TODOapp/core/models/user.py
+++ TODOapp/core/models/user.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -13,5 +14,7 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
     password: Mapped[str]
     active: Mapped[bool] = mapped_column(default=True, server_default="1")
+    name: Mapped[str | None] = mapped_column(String(32))
+    b_date: Mapped[date | None]
 
     tasks: Mapped[list["Task"]] = relationship(back_populates="user")
