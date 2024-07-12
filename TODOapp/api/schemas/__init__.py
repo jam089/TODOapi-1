3f62c78101174f860ff1b313467622f6bc39dccc
jam089 <jam089@mail.ru>
2024-07-12 18:47:47 +0300

    add SearchTaskSchm to init

--- TODOapp/api/schemas/__init__.py
+++ TODOapp/api/schemas/__init__.py
@@ -6,7 +6,14 @@ __all__ = (
     "ChangeTaskUserSchm",
     "UpdateTaskSchm",
     "TaskSchm",
+    "SearchTaskSchm",
 )
 
 from .user import CreateUserSchm, UpdateUserSchm, UserSchm
-from .task import CreateTaskSchm, ChangeTaskUserSchm, UpdateTaskSchm, TaskSchm
+from .task import (
+    CreateTaskSchm,
+    ChangeTaskUserSchm,
+    UpdateTaskSchm,
+    TaskSchm,
+    SearchTaskSchm,
+)
