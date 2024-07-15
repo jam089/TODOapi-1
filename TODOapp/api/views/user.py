f2a95c47568877265d98cc177cc812ba87a347df
jam089 <jam089@mail.ru>
2024-07-15 21:17:46 +0300

    add get_user_by_username endpoint and modify get_all_user to get_all_user_and_by_id

--- TODOapp/api/views/user.py
+++ TODOapp/api/views/user.py
@@ -1,18 +1,15 @@
-from typing import Sequence, Annotated
+from typing import Sequence, Annotated, Union
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
-from api.schemas import UserSchm
+from api.schemas import UserSchm, UserSchmExtended
 from core.crud import user
 
 router = APIRouter()
 
 
-@router.get("/", response_model=Sequence[UserSchm])
-async def get_all_users(
-    session: AsyncSession = Depends(db_helper.session_getter),
 @router.get("/{username}/", response_model=UserSchm)
 async def get_user_by_username(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
@@ -21,5 +18,18 @@ async def get_user_by_username(
     return await user.get_user_by_username(session, username)
 
 
+@router.get(
+    "/",
+    response_model=Union[
+        UserSchmExtended,
+        Sequence[UserSchmExtended],
+    ],
+)
+async def get_all_user_and_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    id: Annotated[int, Path] | None = None,
 ):
+    if id:
+        return await user.get_user_by_id(session, id)
+
     return await user.get_all_users(session)
