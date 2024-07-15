fc7f0661889ddb3f9c234fcce3f0cb416c308824
jam089 <jam089@mail.ru>
2024-07-15 21:14:09 +0300

    add get_user_by_username endpoint

--- TODOapp/api/views/user.py
+++ TODOapp/api/views/user.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, Annotated
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -13,5 +13,13 @@ router = APIRouter()
 @router.get("/", response_model=Sequence[UserSchm])
 async def get_all_users(
     session: AsyncSession = Depends(db_helper.session_getter),
+@router.get("/{username}/", response_model=UserSchm)
+async def get_user_by_username(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    username: str,
+):
+    return await user.get_user_by_username(session, username)
+
+
 ):
     return await user.get_all_users(session)
