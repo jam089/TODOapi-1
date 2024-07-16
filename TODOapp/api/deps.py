a2b049f9414faf0959374fa5d8b7b9b3e43aa5c1
jam089 <jam089@mail.ru>
2024-07-16 03:02:21 +0300

    add get_user deps func

---
+++ TODOapp/api/deps.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends, Path, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from core.models import User as UserModel
+from core.crud import user
+
+
+async def get_user(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user_to_update_id: Annotated[int, Path],
+) -> UserModel:
+    user_to_update = await user.get_user_by_id(session, user_to_update_id)
+    if user_to_update:
+        return user_to_update
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"user with id=[{int(user_to_update_id)}] not found",
+    )
