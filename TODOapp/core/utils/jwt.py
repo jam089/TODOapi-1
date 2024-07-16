444909e196a279bc1be1ac65248f078276a23ffd
jam089 <jam089@mail.ru>
2024-07-16 18:05:43 +0300

    add check_password

--- TODOapp/core/utils/jwt.py
+++ TODOapp/core/utils/jwt.py
@@ -44,3 +44,10 @@ def hash_password(
     password: str,
 ) -> bytes:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def check_password(
+    password: str,
+    hashed_password: bytes,
+) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed_password)
