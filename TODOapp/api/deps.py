1d2660303176a8a28f59b7d3e37b7d6bf3c78461
jam089 <jam089@mail.ru>
2024-07-16 03:20:36 +0300

    go from parameterized req to path

--- TODOapp/api/deps.py
+++ TODOapp/api/deps.py
@@ -10,13 +10,13 @@ from core.crud import user
 
 async def get_user(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-    user_to_update_id: Annotated[int, Path],
+    user_id: Annotated[int, Path],
 ) -> UserModel:
-    user_to_update = await user.get_user_by_id(session, user_to_update_id)
+    user_to_update = await user.get_user_by_id(session, user_id)
     if user_to_update:
         return user_to_update
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"user with id=[{int(user_to_update_id)}] not found",
+        detail=f"user with id=[{int(user_id)}] not found",
     )
--- TODOapp/api/views/user.py
+++ TODOapp/api/views/user.py
@@ -68,7 +68,7 @@ async def create_user(
     )
 
 
-@router.patch("/", response_model=UserSchm)
+@router.patch("/{user_id}/", response_model=UserSchm)
 async def update_user(
     user_input: UpdateUserSchm,
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
@@ -83,7 +83,7 @@ async def update_user(
 
 
 @router.delete(
-    "/",
+    "/{user_id}/",
     status_code=status.HTTP_204_NO_CONTENT,
 )
 async def delete_user(
