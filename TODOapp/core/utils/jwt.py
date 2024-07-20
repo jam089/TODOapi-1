9147dd6d272fb0076f836c303eabff071622cbc8
jam089 <jam089@mail.ru>
2024-07-20 22:23:00 +0300

    change path to authJWT cfg

--- TODOapp/core/utils/jwt.py
+++ TODOapp/core/utils/jwt.py
@@ -8,9 +8,9 @@ from core.config import settings
 
 def encode_jwt(
     payload: dict,
-    private_key: str = settings.auth_jwt.private_key_path.read_text(),
-    algorithm: str = settings.auth_jwt.algorithm,
-    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    private_key: str = settings.api.auth_jwt.private_key_path.read_text(),
+    algorithm: str = settings.api.auth_jwt.algorithm,
+    expire_minutes: int = settings.api.auth_jwt.access_token_expire_minutes,
     expire_timedelta: timedelta | None = None,
 ) -> str:
     to_payload = payload.copy()
@@ -29,8 +29,8 @@ def encode_jwt(
 
 def decode_jwt(
     token: str | bytes,
-    public_key: str = settings.auth_jwt.public_key_path.read_text(),
-    algorithm: str = settings.auth_jwt.algorithm,
+    public_key: str = settings.api.auth_jwt.public_key_path.read_text(),
+    algorithm: str = settings.api.auth_jwt.algorithm,
 ) -> dict:
     decoded = jwt.decode(
         jwt=token,
