15c85303f53d7ad5f9d61a6d18871ba90f0dac25
jam089 <jam089@mail.ru>
2024-07-20 22:25:53 +0300

    add auth router to init

--- TODOapp/api/__init__.py
+++ TODOapp/api/__init__.py
@@ -3,10 +3,16 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .views.user import router as user_router
+from .auth.auth import router as auth_router
 
 router = APIRouter(prefix=settings.api.prefix)
 router.include_router(
     router=user_router,
     prefix=settings.api.user.prefix,
     tags=[settings.api.user.tag],
+)
+router.include_router(
+    router=auth_router,
+    prefix=settings.api.auth_jwt.prefix,
+    tags=[settings.api.auth_jwt.tag],
 )
