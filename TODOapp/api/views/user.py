cdccb234672479c8a0607ed759d640eeef89fbb9
jam089 <jam089@mail.ru>
2024-07-22 03:49:12 +0300

    add auth and admin_auth to endpoints

--- TODOapp/api/views/user.py
+++ TODOapp/api/views/user.py
@@ -3,9 +3,10 @@ from typing import Sequence, Annotated, Union
 from fastapi import APIRouter, Depends, Path, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.auth.validation import get_currant_auth_user
+from api.auth.validation import get_currant_auth_user, get_currant_auth_user_with_admin
 from api.schemas import UserSchm, UserSchmExtended, CreateUserSchm, UpdateUserSchm
 from api import deps
+from core.config import settings
 from core.models import db_helper
 from core.models import User as UserModel
 from core.crud import user
@@ -40,14 +41,23 @@ async def get_user_by_username(
     "/",
     response_model=Union[
         UserSchmExtended,
-        Sequence[UserSchmExtended],
+        Sequence[UserSchm],
     ],
 )
 async def get_all_user_and_by_id(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    current_user: Annotated[
+        UserSchmExtended,
+        Depends(get_currant_auth_user),
+    ],
     id: Annotated[int, Path] | None = None,
 ):
     if id:
+        if current_user.role != settings.roles.admin:
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                detail="not enough privileges",
+            )
         user_by_id: UpdateUserSchm | None = await user.get_user_by_id(session, id)
         if user_by_id:
             return user_by_id
@@ -81,7 +91,29 @@ async def update_user(
     user_input: UpdateUserSchm,
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
     user_to_update: Annotated[UserModel, Depends(deps.get_user)],
+    current_user: Annotated[
+        UserSchmExtended,
+        Depends(get_currant_auth_user_with_admin),
+    ],
 ):
+    if not await user.get_user_by_username(session, user_input.username):
+        return await user.update_user(session, user_to_update, user_input)
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f"{user_input.username} already exist",
+    )
+
+
+@router.patch("/", response_model=UserSchmExtended)
+async def update_yourself(
+    user_input: UpdateUserSchm,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    current_user: Annotated[
+        UserSchmExtended,
+        Depends(get_currant_auth_user),
+    ],
+):
+    user_to_update = await user.get_user_by_id(session, current_user.id)
     if not await user.get_user_by_username(session, user_input.username):
         return await user.update_user(session, user_to_update, user_input)
     raise HTTPException(
@@ -97,5 +129,21 @@ async def update_user(
 async def delete_user(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
     user_to_delete: Annotated[UserModel, Depends(deps.get_user)],
+    current_user: Annotated[
+        UserSchmExtended,
+        Depends(get_currant_auth_user_with_admin),
+    ],
 ) -> None:
     await user.delete_user(session, user_to_delete)
+
+
+@router.delete(
+    "/",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_yourself(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    current_user: Annotated[UserSchmExtended, Depends(get_currant_auth_user)],
+) -> None:
+    user_in_db: UserModel = await user.get_user_by_id(session, current_user.id)
+    await user.delete_user(session, user_in_db)
