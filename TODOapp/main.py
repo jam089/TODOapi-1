2e990de3ad2660b0cb2a13a63d90c2ea061d116e
jam089 <jam089@mail.ru>
2024-07-23 00:41:42 +0300

    add check_and_create_superuser to lifespan

--- TODOapp/main.py
+++ TODOapp/main.py
@@ -6,11 +6,13 @@ import uvicorn
 from core.config import settings
 from core.models import db_helper
 from api import router as api_router
+from core.utils.on_startup_scripts import check_and_create_superuser
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-
+    async with db_helper.session_factory() as session:
+        await check_and_create_superuser(session)
     yield
     await db_helper.dispose()
 
