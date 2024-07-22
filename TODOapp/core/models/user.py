eb7dcd746cfff43fe6a3cc67627d07825c7b28be
jam089 <jam089@mail.ru>
2024-07-22 03:44:17 +0300

    add role to user model

--- TODOapp/core/models/user.py
+++ TODOapp/core/models/user.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
 
 from core.models import Base
+from core.config import settings
 
 if TYPE_CHECKING:
     from core.models import Task
@@ -16,5 +17,9 @@ class User(Base):
     active: Mapped[bool] = mapped_column(default=True, server_default="1")
     name: Mapped[str | None] = mapped_column(String(32))
     b_date: Mapped[date | None]
+    role: Mapped[str] = mapped_column(
+        default=settings.roles.user,
+        server_default=str(settings.roles.user),
+    )
 
     tasks: Mapped[list["Task"]] = relationship(back_populates="user")
