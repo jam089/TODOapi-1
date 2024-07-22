bcace5dc7e0efc49f35a3a0c323ce352649a25ec
jam089 <jam089@mail.ru>
2024-07-23 00:51:59 +0300

    init task view file, create task_router and add it to __init__

--- TODOapp/api/__init__.py
+++ TODOapp/api/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .views.user import router as user_router
+from .views.task import router as task_router
 from .auth.auth import router as auth_router
 
 router = APIRouter(prefix=settings.api.prefix)
@@ -10,6 +11,11 @@ router.include_router(
     router=user_router,
     prefix=settings.api.user.prefix,
     tags=[settings.api.user.tag],
+)
+router.include_router(
+    router=task_router,
+    prefix=settings.api.task.prefix,
+    tags=[settings.api.task.tag],
 )
 router.include_router(
     router=auth_router,
