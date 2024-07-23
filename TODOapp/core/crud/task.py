8fb7289c61afe3cf86e798576a9c12138923d288
jam089 <jam089@mail.ru>
2024-07-24 02:27:31 +0300

    remove get_task_by_user

--- TODOapp/core/crud/task.py
+++ TODOapp/core/crud/task.py
@@ -32,15 +32,6 @@ async def get_task_by_id(session: AsyncSession, task_id: int) -> Task | None:
     return await session.get(Task, task_id)
 
 
-async def get_task_by_user(
-    session: AsyncSession,
-    user: User,
-) -> Sequence[Task] | None:
-    stmt = select(Task).where(Task.user_id == user.id).order_by(Task.id)
-    result: ScalarResult = await session.scalars(stmt)
-    return result.all()
-
-
 async def get_tasks_by_some_statement(
     session: AsyncSession,
     search_task: SearchTaskSchm,
