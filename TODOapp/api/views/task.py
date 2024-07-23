a23eb12f26c7fc7a29b8ea7f9b9ec98c6497f4e8
jam089 <jam089@mail.ru>
2024-07-24 02:31:33 +0300

    replace TaskSchmExtended by TaskSchm, add get_task_by_user_id and get_user_all_tasks endpoints

--- TODOapp/api/views/task.py
+++ TODOapp/api/views/task.py
@@ -5,7 +5,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.auth.validation import get_currant_auth_user_with_admin, get_currant_auth_user
 from api.schemas import TaskSchm, UserSchmExtended
+from api import deps
 from core.models import db_helper
+from core.models.user import User as UserModel
 from core.crud import task as crud
 
 router = APIRouter()
@@ -21,6 +23,24 @@ async def get_all_tasks(
 
 @router.get("/task-id={task_id}", response_model=TaskSchm)
 async def get_task_by_task_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    task_id: int,
+    admin: Annotated[UserSchmExtended, Depends(get_currant_auth_user_with_admin)],
+):
+    return await crud.get_task_by_id(session, task_id)
+
+
+@router.get("/user-id={user_id}", response_model=Sequence[TaskSchm])
+async def get_task_by_user_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[UserModel, Depends(deps.get_user)],
+    admin: Annotated[UserSchmExtended, Depends(get_currant_auth_user_with_admin)],
+):
+    return await crud.get_user_all_tasks(session, UserSchmExtended.model_validate(user))
+
+
+@router.get("/", response_model=Sequence[TaskSchm])
+async def get_user_all_tasks(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
     user: Annotated[UserSchmExtended, Depends(get_currant_auth_user)],
 ):
