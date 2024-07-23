9635b57e21e9eb96ba53dcab356669c786c65f8d
jam089 <jam089@mail.ru>
2024-07-23 17:21:18 +0300

    add get_user_all_tasks endpoint

--- TODOapp/api/views/task.py
+++ TODOapp/api/views/task.py
@@ -3,7 +3,7 @@ from typing import Annotated, Sequence
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.auth.validation import get_currant_auth_user_with_admin
+from api.auth.validation import get_currant_auth_user_with_admin, get_currant_auth_user
 from api.schemas import TaskSchmExtended, UserSchmExtended
 from core.models import db_helper
 from core.crud import task as crud
@@ -17,3 +17,11 @@ async def get_all_tasks(
     admin: Annotated[UserSchmExtended, Depends(get_currant_auth_user_with_admin)],
 ):
     return await crud.get_all_tasks(session)
+
+
+@router.get("/", response_model=Sequence[TaskSchmExtended])
+async def get_all_tasks(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[UserSchmExtended, Depends(get_currant_auth_user)],
+):
+    return await crud.get_user_all_tasks(session, user)
