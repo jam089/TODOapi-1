f4620a79996d8f204264a33ddd063caf648f1c85
jam089 <jam089@mail.ru>
2024-07-24 04:02:10 +0300

    add update_task endpoint

--- TODOapp/api/views/task.py
+++ TODOapp/api/views/task.py
@@ -1,12 +1,13 @@
 from typing import Annotated, Sequence
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.auth.validation import get_currant_auth_user_with_admin, get_currant_auth_user
 from api.schemas import TaskSchm, UserSchmExtended, CreateTaskSchm, UpdateTaskSchm
 from api import deps
-from core.models import db_helper
+from core.config import settings
+from core.models import db_helper, Task
 from core.models.user import User as UserModel
 from core.crud import task as crud
 
@@ -58,3 +59,19 @@ async def create_task(
     user: Annotated[UserSchmExtended, Depends(get_currant_auth_user)],
 ):
     return await crud.create_task(session, task_input, user)
+
+
+@router.patch("/{task_id}/", response_model=TaskSchm)
+async def update_task(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    task_input: UpdateTaskSchm,
+    task_id: int,
+    user: Annotated[UserSchmExtended, Depends(get_currant_auth_user)],
+):
+    task_to_update: Task = await crud.get_task_by_id(session, task_id)
+    if task_to_update.user_id == user.id or user.role == settings.roles.admin:
+        return await crud.update_task(session, task_to_update, task_input)
+    raise HTTPException(
+        status_code=status.HTTP_406_NOT_ACCEPTABLE,
+        detail="not enough privileges",
+    )
