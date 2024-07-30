c8f5a3f9e8132c9135a5e85415729ab5076fe89e
jam089 <jam089@mail.ru>
2024-07-31 00:18:55 +0300

    add jwt_config fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from core.models import db_helper, Base
 
+from core.config import settings
+
 
 @pytest.fixture(scope="session", autouse=True)
 async def prepare_db():
@@ -11,3 +13,14 @@ async def prepare_db():
     yield
     async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="session")
+def jwt_config():
+    jwt_config = {
+        "private_key": settings.api.auth_jwt.private_key_path.read_text(),
+        "public_key": settings.api.auth_jwt.public_key_path.read_text(),
+        "algorithm": settings.api.auth_jwt.algorithm,
+        "expire_minutes": 15,
+    }
+    return jwt_config
