14c89240827eef02872d79eb19c564269a771c4b
jam089 <jam089@mail.ru>
2024-07-31 00:21:23 +0300

    add test_encode_decode_jwt

---
+++ tests/unit_tests/test_utils_jwt.py
@@ -0,0 +1,26 @@
+from datetime import datetime, UTC, timedelta
+
+from core.utils.jwt import encode_jwt, decode_jwt
+
+
+def test_encode_decode_jwt(jwt_payload_example, jwt_config):
+    payload = jwt_payload_example.copy()
+    exp_min = jwt_config.get("expire_minutes")
+    now = datetime.now(UTC)
+    expire = now + timedelta(minutes=exp_min)
+    jwt = encode_jwt(
+        payload=payload,
+        private_key=jwt_config.get("private_key"),
+        algorithm=jwt_config.get("algorithm"),
+        expire_minutes=exp_min,
+    )
+    payload.update(
+        iat=int(datetime.timestamp(now)),
+        exp=int(datetime.timestamp(expire)),
+    )
+    decoded_payload = decode_jwt(
+        token=jwt,
+        public_key=jwt_config.get("public_key"),
+        algorithm=jwt_config.get("algorithm"),
+    )
+    assert decoded_payload == payload
