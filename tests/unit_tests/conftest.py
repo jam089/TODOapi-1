f97b3e8f783b8447a8fefd29eac5d7bbbd2d9e3f
jam089 <jam089@mail.ru>
2024-07-31 00:20:35 +0300

    add jwt_payload_example and passwords fixtures

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def camel_examples():
     camel_examples = {
         "TestClass": "test_class",
@@ -10,3 +10,27 @@ def camel_examples():
         "DoubleTTestClass123": "double_t_test_class123",
     }
     return camel_examples
+
+
+@pytest.fixture(scope="session")
+def jwt_payload_example():
+    payload = {
+        "type": "access",
+        "iss": "john@malkovich.com",
+        "sub": 7,
+        "username": "john",
+        "jti": "15937a7e-2a4f-41aa-9267-ee2ec264069b",
+        "name": "John Malkovich",
+        "logged_in_at": 1722351564.249006,
+        "role": "User",
+    }
+    return payload
+
+
+@pytest.fixture(scope="session")
+def passwords():
+    passwords = [
+        "fg345gGdg",
+        "!432%#$fG",
+    ]
+    return passwords
