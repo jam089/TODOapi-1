1035daccde05bbf1043947b12522d937c4ff1038
jam089 <jam089@mail.ru>
2024-08-01 04:04:53 +0300

    add test_engine, test_session_factory and overriding test_session_factory, event_loop func

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,57 @@
+import asyncio
+from typing import AsyncGenerator
+
 import pytest
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from core.models import db_helper, Base
 
 from core.config import settings
+from main import todo_app
+
+test_engine = create_async_engine(
+    url=str(settings.db.url),
+    poolclass=NullPool,
+)
+test_session_factory = async_sessionmaker(
+    bind=test_engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+)
+
+
+async def override_dispose() -> None:
+    await test_engine.dispose()
+
+
+async def override_session_getter() -> AsyncGenerator[AsyncSession, None]:
+    async with test_session_factory() as session:
+        yield session
+
+
+todo_app.dependency_overrides[db_helper.session_getter] = override_session_getter
+todo_app.dependency_overrides[db_helper.dispose] = override_dispose
 
 
 @pytest.fixture(scope="session", autouse=True)
 async def prepare_db():
     assert settings.db.mode == "TEST"
-    async with db_helper.engine.begin() as conn:
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
-    async with db_helper.engine.begin() as conn:
+    async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="session")
+def event_loop(request):
+    loop = asyncio.get_event_loop_policy().get_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope="session")
