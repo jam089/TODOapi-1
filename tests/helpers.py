06e13914f4376277d2a7bfc66c0b70fb876b3b2c
jam089 <jam089@mail.ru>
2024-08-02 19:59:21 +0300

    add TestUser class

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,3 +1,40 @@
+class TestUser:
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        update_testcase: dict,
+        new_password: str,
+        name: str | None = None,
+        b_date: str | None = None,
+    ):
+        self.username = username
+        self.password = password
+        self.update_testcase = update_testcase
+        self._new_password = new_password
+        self.name = name
+        self.b_date = b_date
+
+    def update_user(self):
+        if username := self.update_testcase.get("username"):
+            self.username = username
+        if name := self.update_testcase.get("name"):
+            self.name = name
+        if b_date := self.update_testcase.get("b_date"):
+            self.b_date = b_date
+
+    def update_password(self):
+        self.password = self._new_password
+
+    def json(self):
+        return {
+            "username": self.username,
+            "name": self.name,
+            "b_date": self.b_date,
+            "password": self.password,
+        }
+
+
 class AuthedUser:
     def __init__(self, username, password, access_token, refresh_token):
         self.username = username
