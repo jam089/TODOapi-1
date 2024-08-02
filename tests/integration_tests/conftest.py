7c3fab9b333f6be1d1cd0ec0941868b88de47d8e
jam089 <jam089@mail.ru>
2024-08-02 20:15:03 +0300

    change testcase getting method from generator to TestUser class

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -4,13 +4,43 @@ import pytest
 from httpx import AsyncClient, ASGITransport
 
 from core.config import settings
+from core.utils.on_startup_scripts import check_and_create_superuser
 from main import todo_app
 
-from tests.helpers import AuthedUser
+from tests.helpers import AuthedUser, TestUser
+
+
+test_user_jack = TestUser(
+    username="jack_n",
+    name="Jack Nicholson",
+    b_date="1937-04-22",
+    password="jack",
+    new_password="jack1234",
+    update_testcase={
+        "username": "jack_nicholson",
+        "name": "Jack Nicholson",
+        "b_date": "2000-12-01",
+        "active": True,
+    },
+)
+
+test_user_john = TestUser(
+    username="john_doe",
+    password="john_doe",
+    new_password="john1234",
+    update_testcase={
+        "username": "john_doe2",
+    },
+)
+
+test_users = [test_user_jack, test_user_john]
 
 
 @pytest.fixture(scope="session")
 async def async_client() -> AsyncGenerator[AsyncClient, None]:
+    """
+    Creating async test client
+    """
     async with AsyncClient(
         transport=ASGITransport(todo_app),
         base_url=f"http://test{settings.api.prefix}",
@@ -20,41 +50,37 @@ async def async_client() -> AsyncGenerator[AsyncClient, None]:
         yield ac
 
 
-def users_for_tests():
-    users = [
-        {
-            "username": "jack_n",
-            "name": "Jack Nicholson",
-            "b_date": "1937-04-22",
-            "password": "jack",
-        },
-        {
-            "username": "john_doe",
-            "password": "john_doe",
-        },
-    ]
-    for user in users:
-        yield user
+@pytest.fixture(scope="session", autouse=True)
+async def create_superuser(test_session):
+    """
+    Creating superuser for admin access rights checking
+    """
+    await check_and_create_superuser(test_session)
 
 
-@pytest.fixture(params=users_for_tests())
-def for_sequenced_user_tests(request):
+@pytest.fixture(scope="session", params=test_users)
+def for_sequenced_user_tests(request) -> TestUser:
+    """
+    Fixture which return one test user for sequence of tests
+    """
     return request.param
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 async def auth_user(async_client: AsyncClient, for_sequenced_user_tests):
+    """
+    Get loging test users
+    """
     request_json = {
-        "username": for_sequenced_user_tests.get("username"),
-        "password": for_sequenced_user_tests.get("password"),
+        "username": for_sequenced_user_tests.username,
+        "password": for_sequenced_user_tests.password,
     }
     response = await async_client.post(
         url=f"{settings.api.auth_jwt.prefix}/login/",
         data=request_json,
     )
     return AuthedUser(
-        username=for_sequenced_user_tests.get("username"),
-        password=for_sequenced_user_tests.get("password"),
+        user=for_sequenced_user_tests,
         access_token=response.json().get("access_token"),
         refresh_token=response.json().get("refresh_token"),
     )
