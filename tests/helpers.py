da5d864e81aee8c5437a7e2d3dd0e31197a6b53e
jam089 <jam089@mail.ru>
2024-08-03 01:58:06 +0300

    add update_by_admin_testcase and add it to update_user method for TestUser class

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,6 +4,7 @@ class TestUser:
         username: str,
         password: str,
         update_testcase: dict | None = None,
+        update_by_admin_testcase: dict | None = None,
         new_password: str = None,
         name: str | None = None,
         b_date: str | None = None,
@@ -11,13 +12,22 @@ class TestUser:
         self.username = username
         self.password = password
         self.update_testcase = update_testcase
+        self.update_by_admin_testcase = update_by_admin_testcase
         self._new_password = new_password
         self.name = name
         self.b_date = b_date
         self.user_id: int | None = None
 
-    def update_user(self):
-        if self.update_testcase:
+    def update_user(self, admin_flg: bool = False):
+        if self.update_by_admin_testcase and admin_flg:
+            if username := self.update_by_admin_testcase.get("username"):
+                self.username = username
+            if name := self.update_by_admin_testcase.get("name"):
+                self.name = name
+            if b_date := self.update_by_admin_testcase.get("b_date"):
+                self.b_date = b_date
+            return True
+        elif self.update_testcase:
             if username := self.update_testcase.get("username"):
                 self.username = username
             if name := self.update_testcase.get("name"):
