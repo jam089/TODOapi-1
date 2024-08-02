310c83bb1f3e844a9456aff3dc67d6042477dc6a
jam089 <jam089@mail.ru>
2024-08-02 20:16:36 +0300

    change testcase getting method from generator to TestUser class

--- tests/integration_tests/test_users.py
+++ tests/integration_tests/test_users.py
@@ -3,37 +3,42 @@ from httpx import AsyncClient
 
 from core.config import settings
 
-from tests.integration_tests.conftest import users_for_tests
+from tests.integration_tests.conftest import test_users
 
 
-@pytest.mark.parametrize("user", users_for_tests())
+@pytest.mark.parametrize("user", test_users)
 async def test_endpoint_create_user(async_client: AsyncClient, user):
     response = await async_client.post(
         url=f"{settings.api.user.prefix}/",
-        json=user,
+        json=user.json(),
     )
     assert response.status_code == 201
-    assert response.json().get("username") == user.get("username")
-    assert response.json().get("name") == user.get("name")
-    assert response.json().get("b_date") == user.get("b_date")
+    assert response.json().get("username") == user.username
+    assert response.json().get("name") == user.name
+    assert response.json().get("b_date") == user.b_date
     assert response.json().get("active"), "active not exist"
 
 
-@pytest.mark.parametrize("user", users_for_tests())
+@pytest.mark.parametrize("user", test_users)
 async def test_endpoint_get_user_by_username(async_client: AsyncClient, user):
     response = await async_client.get(
-        url=f"{settings.api.user.prefix}/{user.get("username")}/",
+        url=f"{settings.api.user.prefix}/{user.username}/",
     )
     assert response.status_code == 200
-    assert response.json().get("username") == user.get("username")
-    assert response.json().get("name") == user.get("name")
-    assert response.json().get("b_date") == user.get("b_date")
+    assert response.json().get("username") == user.username
+    assert response.json().get("name") == user.name
+    assert response.json().get("b_date") == user.b_date
     assert response.json().get("active"), "active not exist"
 
 
 async def test_endpoint_get_profile(async_client: AsyncClient, auth_user):
     response = await async_client.get(
         url=f"{settings.api.user.prefix}/profile/",
+        headers=auth_user.headers,
+    )
+    assert response.status_code == 200
+    assert response.json().get("created_at"), "created_at not exist"
+    assert response.json().get("role"), "role not exist"
         headers=auth_user.headers,
     )
     assert response.status_code == 200
