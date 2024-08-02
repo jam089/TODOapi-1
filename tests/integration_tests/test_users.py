9310d7258afd9228a4db1411a2b44145e94af4d3
jam089 <jam089@mail.ru>
2024-08-03 02:13:50 +0300

    add test_admin_endpoint_change_role

--- tests/integration_tests/test_users.py
+++ tests/integration_tests/test_users.py
@@ -113,6 +113,28 @@ async def test_endpoint_change_your_password(async_client, auth_user):
             auth_user.user.update_password()
 
 
+async def test_admin_endpoint_change_role(
+    async_client,
+    auth_superuser,
+    for_sequenced_user_tests,
+):
+    response = await async_client.patch(
+        url=f"{settings.api.user.prefix}/{for_sequenced_user_tests.user_id}/role/",
+        json={"role": settings.roles.admin},
+        headers=auth_superuser.headers,
+    )
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json().get("username") == for_sequenced_user_tests.username
+    assert response.json().get("name") == for_sequenced_user_tests.name
+    assert response.json().get("b_date") == for_sequenced_user_tests.b_date
+    assert response.json().get("id") == for_sequenced_user_tests.user_id
+    assert response.json().get("role") == settings.roles.admin
+    assert response.json().get("created_at"), "'created_at' filed not exist"
+    assert response.json().get("last_update_at"), "'last_update_at' filed not exist"
+    assert response.json().get("active"), "'active' filed not exist"
+
+
 async def test_endpoint_delete_yourself(async_client, auth_user):
     response = await async_client.delete(
         url=f"{settings.api.user.prefix}/",
