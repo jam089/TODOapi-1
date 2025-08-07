3f20905698b47bd6dfbd53a52a85ed6983f88eea
Ivan Filippov <jam089@mail.ru>
2025-08-07 18:11:42 +0300

    add fixtures for users and tasks mocks

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
+from core.models import User, Task
 
-@pytest.fixture(scope="session")
+
+@pytest.fixture(scope="package")
 def jwt_payload_example():
     payload = {
         "type": "access",
@@ -14,3 +16,35 @@ def jwt_payload_example():
         "role": "User",
     }
     return payload
+
+
+@pytest.fixture(scope="package")
+def user_mock():
+    user_list = [
+        User(id=0, name="Test User 1"),
+        User(id=1, name="Test User 2"),
+    ]
+
+    def _create_user(user_id):
+        try:
+            return user_list[user_id]
+        except IndexError:
+            return None
+
+    return _create_user
+
+
+@pytest.fixture(scope="package")
+def task_mock():
+    task_list = [
+        Task(id=0, name="Test Task 1"),
+        Task(id=1, name="Test Task 2"),
+    ]
+
+    def _create_task(task_id):
+        try:
+            return task_list[task_id]
+        except IndexError:
+            return None
+
+    return _create_task
