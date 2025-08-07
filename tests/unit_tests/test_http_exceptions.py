931f8aafbae01cf016369713ea00649c459c2db2
Ivan Filippov <jam089@mail.ru>
2025-08-07 12:28:30 +0300

    create test_http_exceptions.py and add 3 tests for rendering_exception_with_param

---
+++ tests/unit_tests/test_http_exceptions.py
@@ -0,0 +1,33 @@
+import pytest
+from fastapi import HTTPException, status
+
+from api.http_exceptions import rendering_exception_with_param
+
+
+def test_rendering_exception_with_valid_template():
+    test_exc_with_param = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Test error with $parameter not found",
+    )
+    result = rendering_exception_with_param(test_exc_with_param, "bugs")
+    assert result.detail == "Test error with bugs not found"
+    assert result.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_rendering_exception_with_no_param_template():
+    test_exc_without_param = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Test error not found",
+    )
+    result = rendering_exception_with_param(test_exc_without_param, "bugs")
+    assert result.detail == "Test error not found"
+    assert result.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_rendering_exception_with_invalid_template():
+    test_exc_invalid = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Test error with $parameter and $wrong",
+    )
+    with pytest.raises(KeyError):
+        rendering_exception_with_param(test_exc_invalid, "bugs")
