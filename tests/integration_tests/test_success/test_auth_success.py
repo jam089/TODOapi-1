fadc65b226de455941aba5902144c5655129904a
Ivan Filippov <jam089@mail.ru>
2025-09-24 11:34:32 +0300

    add test_endpoint_auth_user_refresh

--- tests/integration_tests/test_success/test_auth_success.py
+++ tests/integration_tests/test_success/test_auth_success.py
@@ -6,14 +6,14 @@ from core.config import settings
 
 @pytest.mark.asyncio
 async def test_endpoint_auth_user_login(
-    async_client: AsyncClient,
+    auth_client: AsyncClient,
     test_user,
 ):
     login_data = {
         "username": test_user.get("user").username,
         "password": test_user.get("password"),
     }
-    response = await async_client.post(
+    response = await auth_client.post(
         url=f"{settings.api.auth_jwt.prefix}/login/",
         data=login_data,
     )
@@ -21,4 +21,18 @@ async def test_endpoint_auth_user_login(
     assert response.status_code == 200
     assert "access_token" in response.json().keys()
     assert "refresh_token" in response.json().keys()
+    assert "set-cookie" in response.headers
+
+
+@pytest.mark.asyncio
+async def test_endpoint_auth_user_refresh(
+    auth_client: AsyncClient,
+    test_user,
+):
+    response = await auth_client.post(
+        url=f"{settings.api.auth_jwt.prefix}/refresh/",
+    )
+
+    assert response.status_code == 200
+    assert "access_token" in response.json().keys()
     assert "set-cookie" in response.headers
