8048f8b3b898b788aa86db862b170d6328e52b3c
Ivan Filippov <jam089@mail.ru>
2025-09-24 19:30:24 +0300

    add TaskFactory

--- tests/integration_tests/factories.py
+++ tests/integration_tests/factories.py
@@ -1,8 +1,10 @@
+from datetime import timedelta
 from typing import Type
 
 import factory
 
-from core.models import User
+from core.config import settings
+from core.models import User, Task
 from tests.integration_tests.database import test_session_factory
 
 
@@ -16,7 +18,20 @@ class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     b_date = factory.Faker("date_of_birth")
 
 
-async def create(factory_class: factory.alchemy.SQLAlchemyModelFactory, **kwargs):
+class TaskFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Task
+        sqlalchemy_session_persistence = "flush"
+
+    name = factory.Faker("catch_phrase")
+    description = factory.Faker("paragraph", nb_sentences=3)
+    start_at = factory.Faker("date_time_this_year")
+    end_at = factory.LazyAttribute(lambda o: o.start_at + timedelta(hours=2))
+    scheduled_hours = factory.Faker("random_int", min=1, max=16)
+    status = factory.Iterator(settings.tstat.model_dump().values())
+    user = factory.SubFactory(UserFactory)
+
+
 async def create(factory_class: Type[factory.alchemy.SQLAlchemyModelFactory], **kwargs):
     obj = factory_class.build(**kwargs)
     async with test_session_factory() as session:
