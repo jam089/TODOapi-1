2de5e28381aa054ccc38d3dd3e53f1bd93476e80
Ivan Filippov <jam089@mail.ru>
2025-09-25 22:07:16 +0300

    fixtures for mutate test task

--- tests/integration_tests/test_failure/conftest.py
+++ tests/integration_tests/test_failure/conftest.py
@@ -18,6 +18,10 @@ VALUE_STATEMENTS = [
     "wrong_role_to_request",
     "wrong_password",
     "wrong_username",
+    "user_id_for_task",
+    "task_id_for_task",
+    "wrong_json",
+    "wrong_status",
 ]
 
 
@@ -25,7 +29,7 @@ async def mutated(
     request,
     test_session: AsyncSession,
     async_client: AsyncClient,
-    user: dict,
+    obj_dict: dict,
 ):
     """
     Examples:
@@ -37,31 +41,33 @@ async def mutated(
     target = wrong_param.get("target")
 
     if target == "user":
-        user_obj: User = user.get("user")
+        user_obj: User = obj_dict.get("user")
+        user_obj = await test_session.merge(user_obj)
         attrs: dict = wrong_param.get("attrs")
+        await test_session.refresh(user_obj)
         for attr, value in attrs.items():
             setattr(user_obj, attr, value)
         await test_session.commit()
         await test_session.refresh(user_obj)
         if "role" in attrs.keys():
             new_auth_info = await authentication(
-                async_client, user_obj, user.get("password")
+                async_client, user_obj, obj_dict.get("password")
             )
-            user.update(new_auth_info)
+            obj_dict.update(new_auth_info)
 
     elif target in VALUE_STATEMENTS:
         value = wrong_param.get("value")
-        user[target] = value
+        obj_dict[target] = value
         if target == "access_token":
-            user["headers"] = {"Authorization": f"Bearer {value}"}
+            obj_dict["headers"] = {"Authorization": f"Bearer {value}"}
 
     elif target == "headers_none":
-        user["headers"] = None
+        obj_dict["headers"] = None
 
     elif target in FLG_STATEMENTS:
-        user[target] = True
+        obj_dict[target] = True
 
-    return user
+    return obj_dict
 
 
 @pytest.fixture
@@ -72,3 +78,15 @@ async def mutated_user(request, test_session, auth_client, test_user):
 @pytest.fixture
 async def mutated_admin(request, test_session, auth_client, admin_user):
     return await mutated(request, test_session, auth_client, admin_user)
+
+
+@pytest.fixture
+async def mutated_task(request, test_session, auth_client, test_task):
+    return await mutated(request, test_session, auth_client, test_task)
+
+
+@pytest.fixture
+async def mutated_multiple_task(
+    request, test_session, auth_client, test_multiple_tasks
+):
+    return await mutated(request, test_session, auth_client, test_multiple_tasks)
