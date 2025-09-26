1abf213e774d73820d2e5517c91f739510c5d1b5
Ivan Filippov <jam089@mail.ru>
2025-09-26 15:32:04 +0300

    test_prestart.sh for test 5

--- tests/test_prestart.sh
+++ tests/test_prestart.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "Creating ./certs dir."
-mkdir ../TODOapp/certs
+mkdir ./TODOapp/certs
 echo "Start generation public/private key pair.."
-openssl genpkey -algorithm RSA -out ../TODOapp/certs/jwt-private.pem -pkeyopt rsa_keygen_bits:2048
+openssl genpkey -algorithm RSA -out ./TODOapp/certs/jwt-private.pem -pkeyopt rsa_keygen_bits:2048
 echo "Private key created."
-openssl rsa -pubout -in ./certs/jwt-private.pem -out ../TODOapp/certs/jwt-public.pem
+openssl rsa -pubout -in ./TODOapp/certs/jwt-private.pem -out ./TODOapp/certs/jwt-public.pem
 echo "Public key created."
 
 exec "$@"
